6d59dc2682f855cc88d4ce2c0a34d06c2db0e9d1
hoandinh <hoandinh.ptit02@gmail.com>
2026-03-29 09:27:52 +0700

    Implement FCM notifications (excluding secret service account key)

--- medicine_app/android/app/build.gradle.kts
+++ medicine_app/android/app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -27,10 +28,11 @@ android {
         applicationId = "com.example.medicine_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = flutter.minSdkVersion // Required for some modern plugins, safe for most apps
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -44,4 +46,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
